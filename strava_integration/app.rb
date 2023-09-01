8b5610c9d547bb5d5fdbd4757a81c6d5675e8df0
Tobiasz Waszak <tobiaszwaszak@gmail.com>
2023-09-01 12:08:17 +0200

    Get activities from Strava

--- strava_integration/app.rb
+++ strava_integration/app.rb
@@ -8,6 +8,8 @@ module StravaIntegration
         Controller.new.authorize(request)
       elsif request.path.include?("/callback") && request.request_method == "GET"
         Controller.new.callback(request)
+      elsif request.path.include?("/get_activities") && request.request_method == "GET"
+        Controller.new.get_activities(request)
       else
         [404, {"Content-Type" => "text/plain"}, ["Not Found"]]
       end
--- strava_integration/repository.rb
+++ strava_integration/repository.rb
@@ -8,5 +8,20 @@ module StravaIntegration
         refresh_token: refresh_token
       )
     end
+
+    def get_access_token
+      Db::Records::StravaCredential.last.access_token
+    end
+
+    def get_refresh_token
+      Db::Records::StravaCredential.last.refresh_token
+    end
+
+    def update_credentials(access_token:, refresh_token:)
+      Db::Records::StravaCredential.last.update(
+        access_token: access_token,
+        refresh_token: refresh_token
+      )
+    end
   end
 end
